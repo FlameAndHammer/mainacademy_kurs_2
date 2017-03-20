c1c0f986c21cbf63299b0dcbc19953813e8b1046
Arthur-Konon <chikart@ukr.net>
2017-03-20 20:43:43 +0200

    laba 2_1

--- kurs/src/artur/labs/l_2_1/Computer.java
+++ kurs/src/artur/labs/l_2_1/Computer.java
@@ -2,9 +2,20 @@ package artur.labs.l_2_1;
 
 /**
  * Created by Arthur on 20.03.2017.
+ * Write Computer class, the attributes of this class
+ * is manufacturer (of String type), serialNumber (of int type),
+ * price (of float type),
+ * quantityCPU (of int type) and frequencyCPU (of int type).
  */
 public class Computer {
+
+        private String manufacturer;
+        private int serialNumber;
+        private float price;
+        private int quantityCPU;
+        private int frequencyCPU;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }
