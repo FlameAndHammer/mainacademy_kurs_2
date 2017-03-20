cadbdf60720b096bec6855a1c21a5be8f0603859
Nikulin Alexander <shas@email.ua>
2017-03-20 20:26:34 +0200

    add slavik

---
+++ kurs/src/_nikulin/labs/l_2_1/Computer.java
@@ -0,0 +1,10 @@
+package _nikulin.labs.l_2_1;
+
+/**
+ * Created by Александр on 20.03.2017.
+ */
+public class Computer {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
