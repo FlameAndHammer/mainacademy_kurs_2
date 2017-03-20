dcb71245b4042becc16fa683185d2e9a5bae554d
Shevchuk Kirill <shevchuc@rambler.ru>
2017-03-20 20:49:34 +0200

    Kirill

--- kurs/src/kirill/labs/l_2_1/Computer.java
+++ kurs/src/kirill/labs/l_2_1/Computer.java
@@ -4,7 +4,9 @@ package kirill.labs.l_2_1;
  * Created by Storm on 20.03.2017.
  */
 public class Computer {
-    public static void main(String[] args) {
-
-    }
+    String manufacturer;
+    int serialNumber;
+    float price;
+    int quantityCPU;
+    int frequencyCPU;
 }
