896e1bb45b58af135b23672d18afdddfddcacf2d
Nikulin Alexander <shas@email.ua>
2017-03-27 19:05:08 +0300

    Lab 2_1 example

---
+++ kurs/src/_nikulin/labs/lab_2_1/Computer.java
@@ -0,0 +1,51 @@
+package _nikulin.labs.lab_2_1;
+
+public class Computer {
+        // lab 2_1_1
+    private String manufacturer;
+    private int serialNumber;
+    private double price;
+    private int  quantityCPU;
+    private int frequencyCPU;
+
+        // lab 2_1_2
+    public String getManufacturer() {
+        return manufacturer;
+    }
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getQuantityCPU() {
+        return quantityCPU;
+    }
+    public void setQuantityCPU(int quantityCPU) {
+        this.quantityCPU = quantityCPU;
+    }
+    public int getFrequencyCPU() {
+        return frequencyCPU;
+    }
+    public void setFrequencyCPU(int frequencyCPU) {
+        this.frequencyCPU = frequencyCPU;
+    }
+
+        // lab 2_1_5
+    void view() {
+        System.out.print("Manufacturer: " + manufacturer);
+        System.out.print("; SerialNumber: " + serialNumber);
+        System.out.print("; Price: " + price);
+        System.out.print("; QuantityCPU: " + quantityCPU);
+        System.out.println("; FrequencyCPU: " + frequencyCPU);
+    }
+}
---
+++ kurs/src/_nikulin/labs/lab_2_1/Main.java
@@ -0,0 +1,71 @@
+package _nikulin.labs.lab_2_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+            // lab 2-1-1
+        Computer object = new Computer();
+        System.out.println(object);
+//        System.out.println(object.manufacturer);  // error
+
+            // lab 2-1-2
+        System.out.println("-------------------------------------------");
+        System.out.println("FrequencyCPU: " + object.getFrequencyCPU());
+        System.out.println("Manufacturer: " + object.getManufacturer());
+        System.out.println("Price: " + object.getPrice());
+        object.setPrice(100.6);
+        System.out.println("New price: " + object.getPrice());
+
+            // lab 2-1-3
+        System.out.println("-------------------------------------------");
+        Computer [] arrayComputer = new Computer[5];
+        for (int i=0; i<arrayComputer.length; i++) {
+            arrayComputer[i] = new Computer();
+            arrayComputer[i].setSerialNumber(10002015 + i);
+        }
+        int number = 1;
+        for(Computer obj : arrayComputer)
+            System.out.println("SerialNumber computer " + (number++) + ": " + obj.getSerialNumber());
+
+        // lab 2-1-4
+        System.out.println("-------------------------------------------");
+        for (int i=0; i<arrayComputer.length; i++) {
+             arrayComputer[i].setPrice(111 + i*10 - i);
+        }
+        for(Computer obj : arrayComputer) {
+            System.out.print("SerialNumber computer " + ": " + obj.getSerialNumber());
+            System.out.println("; Price: " + obj.getPrice());
+        }
+        for (int k=0; k<arrayComputer.length; k++) {
+            double price = arrayComputer[k].getPrice();
+            price += price*0.1;
+            arrayComputer[k].setPrice(price);
+        }
+        System.out.println("\nAfter change:");
+        for(Computer obj : arrayComputer) {
+            System.out.print("SerialNumber computer " + ": " + obj.getSerialNumber());
+            System.out.println("; Price: " + obj.getPrice());
+        }
+
+            // lab 2-1-5
+        System.out.println("-------------------------------------------");
+        Computer [] arrayComputers = new Computer[5];
+        for (int i=0; i<arrayComputers.length; i++) {
+            arrayComputers[i] = new Computer();
+            arrayComputers[i].setSerialNumber(10002015 + i);
+            if (i % 2 == 0) {
+                arrayComputers[i].setManufacturer("ASUS");
+            } else {
+                arrayComputers[i].setManufacturer("HP");
+            }
+            arrayComputers[i].setPrice(111 + i*10 - i);
+            arrayComputers[i].setQuantityCPU(4);
+            arrayComputers[i].setFrequencyCPU(1 + 5 / (i+1));
+        }
+        int counter = 1;
+        for(Computer obj : arrayComputers) {
+            System.out.print("Computer " + counter++ + " -> ");
+            obj.view();
+        }
+    }
+}
