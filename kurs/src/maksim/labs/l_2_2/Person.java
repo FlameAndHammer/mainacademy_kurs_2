3ecb42453b2cfe09b88b1cc611e58bfc84374da9
WIXVW <max@grabov.com>
2017-03-30 23:28:14 +0300

    Grabov M. Lab 2.2

---
+++ kurs/src/maksim/labs/l_2_2/Person.java
@@ -0,0 +1,10 @@
+package maksim.labs.l_2_2;
+
+/**
+ * Created by Пк on 30.03.2017.
+ */
+public class Person {
+    public Person(String firstName, String lastName, int age, String gender, int phoneNumber){
+
+    }
+}
