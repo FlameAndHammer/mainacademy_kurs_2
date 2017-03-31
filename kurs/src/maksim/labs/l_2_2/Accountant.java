6989027588cc27885947984b4aa0cb89801ceb18
WIXVW <max@grabov.com>
2017-03-31 16:05:07 +0300

    Grabov M. Lab 2.2

---
+++ kurs/src/maksim/labs/l_2_2/Accountant.java
@@ -0,0 +1,23 @@
+package maksim.labs.l_2_2;
+import java.util.Random;
+/**
+ * Created by Пк on 30.03.2017.
+ */
+public class Accountant {
+    public static void main(String[] args) {
+        String name = "";
+        for (int i = 0; i < (int) (3 + (Math.random() * 10)); i++) {
+            Random r = new Random();
+            char c = (char) (r.nextInt(26) + 'a');
+            name += c;
+        }
+        Employee empl = new Employee();
+        double salary = 0;
+        for (int i = 1; i <= 12; i++) {
+            salary = (Math.random() * 1000);
+            empl.calcSalary(name, salary);
+            System.out.println(salary);
+        }
+        System.out.println(name + " " + empl.calcSalary(name, salary));
+    }
+}
