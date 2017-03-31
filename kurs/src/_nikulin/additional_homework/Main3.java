daade2d0e4562ba1afdbac6ebf2324ba4a87c5ac
Nikulin Alexander <shas@email.ua>
2017-03-31 21:11:00 +0300

    lecture 31.03.2017

--- kurs/src/_nikulin/additional_homework/Main3.java
+++ kurs/src/_nikulin/additional_homework/Main3.java
@@ -3,18 +3,19 @@ package _nikulin.additional_homework;
 /**
  * Created by nikulin on 3/31/2017.
  */
-class Employee3 {
+class Employee4 {
 
-  Employee3(String str) {
+  Employee4(String str) {
+
     System.out.println("ConstructorStr");
   }
-  /*Employee3() {
-    this();
+  Employee4() {
+
     System.out.println("ConstructorNoArg");
-  }*/
+  }
 }
 public class Main3 {
   public static void main(String[] args) {
-    Employee3 e = new Employee3("Java");
+    Employee4 e = new Employee4("Java");
   }
 }
--- kurs/src/maksim/labs/l_2_2/Employee.java
+++ kurs/src/maksim/labs/l_2_2/Employee.java
@@ -4,14 +4,20 @@ package maksim.labs.l_2_2;
  * Created by Пк on 30.03.2017.
  */
 public class Employee {
+    double totalSalary;
     public double calcSalary(String name, double... salary){
-        double totalSalary = 0;
+         //totalSalary = 0;
         for (double x : salary) {
             totalSalary += x;
         }
         return totalSalary;
 
     }
+
+    public double getTotalSalary() {
+        return totalSalary;
+    }
+
     public void view(){
 
     }
