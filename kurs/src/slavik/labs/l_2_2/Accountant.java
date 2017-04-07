56c8e9d54fc1985c39bb6a8247d15b32400094c4
Viacheslav Dronikov <dvd@te.net.ua>
2017-04-07 13:13:51 +0300

    Laba 2.2

---
+++ kurs/src/slavik/labs/l_2_2/Accountant.java
@@ -0,0 +1,9 @@
+package slavik.labs.l_2_2;
+public class Accountant {
+    public static void main(String[] args) {
+    Employee object = new Employee();
+    object.calcSalary ("Коля", 50, 30.5);
+    object.calcSalary ("Вася", 6, 370.5, 376, 45, 87 ,98);
+    object.calcSalary ("Саня", 0.8, 0.5, 3, 5, 40, 58);
+    }
+}
