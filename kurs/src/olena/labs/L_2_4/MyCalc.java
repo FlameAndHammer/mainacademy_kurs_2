47c1683c8c80b738bbd18f88a7003738f6353ccd
elena fostachuk <elfiora2@gmail.com>
2017-04-07 19:15:04 +0200

    do it

--- kurs/src/olena/labs/L_2_4/MyCalc.java
+++ kurs/src/olena/labs/L_2_4/MyCalc.java
@@ -4,19 +4,24 @@ import java.util.Scanner;
 /**
  * Created by elenafostachuk on 4/7/17.
  */
+import java.util.Scanner;
 public class MyCalc {
-    public static double calcPI(int n) {
-        double s = 0;
-        for (int i = 3; i <= n; i++) {
-            s = 4 / i + 2 ;
+    public static double calcPI(int count) {
+        double pi = 0;
+        final int valFour = 4;
+        double divider = 1.0;
+        for (int i = 1; i <= count; i++, divider += 2) {
+            if (i % 2 == 0)
+                pi += -valFour / divider;
+            else
+                pi += valFour / divider;
         }
-        double pi = 4 + s;
-        s++;
         return pi;
     }
 
+
     public static void main(String[] args) {
-        System.out.print("Input any number");
+        System.out.print("Input some number");
         Scanner number = new Scanner(System.in);
         int n = number.nextInt();
         System.out.println(calcPI(n));
