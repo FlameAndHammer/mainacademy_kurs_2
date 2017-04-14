431c395e80bd145c3ba133115fdb68fc433dbdb0
elena fostachuk <elfiora2@gmail.com>
2017-04-14 14:29:57 +0200

    New changelist

---
+++ kurs/src/olena/labs/L_2_5_1/Main.java
@@ -0,0 +1,10 @@
+package olena.labs.L_2_5_1;
+
+/**
+ * Created by elenafostachuk on 4/14/17.
+ */
+public class Main {
+    public static void main(String[] args){
+        MyInit myInitF = new MyInit();
+    }
+}
