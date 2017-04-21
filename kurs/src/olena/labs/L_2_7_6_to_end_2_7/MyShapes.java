7b7e2778ee3f5bbf1326fd346c209fd2db30da2c
elena fostachuk <elfiora2@gmail.com>
2017-04-21 11:41:32 +0200

    New changelist

---
+++ kurs/src/olena/labs/L_2_7_6_to_end_2_7/MyShapes.java
@@ -0,0 +1,15 @@
+package olena.labs.L_2_7_6_to_end_2_7;
+
+/**
+ * Created by elenafostachuk on 4/21/17.
+ */
+public class MyShapes {
+    private String myShapecolor;
+    public MyShapes(String myShapecolor){this.myShapecolor = myShapecolor;}
+
+    public double calcArea() {return  0.0;}
+
+    @Override
+    public String toString(){
+        return   getClass().getSimpleName() +  "color: "  + myShapecolor;}
+}
