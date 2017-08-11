219c64e999b99747984c628bd66ebcb0edf6ebd6
amartynov <amartynov@lohika.com>
2017-08-11 21:40:16 +0300

    gui example

---
+++ kurs/src/_nikulin/gui/Main.java
@@ -0,0 +1,35 @@
+package _nikulin.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("my prog");
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 50, 50));
+        frame.add(panel);
+
+        JTextField textField = new JTextField();
+        textField.setColumns(20);
+        panel.add(textField);
+
+        JLabel label = new JLabel("text");
+
+        JButton btn = new JButton("Key: ");
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText(textField.getText());
+            }
+        });
+        panel.add(btn);
+        panel.add(label);
+
+        frame.setVisible(true);
+    }
+}
